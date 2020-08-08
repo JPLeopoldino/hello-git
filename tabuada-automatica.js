4ae82dab1801a3ed26a5ad5896e49bbc0a27fa2e
João Pedro <leopoldino26@gmail.com>
2020-08-08 12:09:01 -0300

    Tábuada Automática v0.1

---
+++ tabuada-automatica.js
@@ -0,0 +1,9 @@
+let numeroMultiplicar = 0;
+//---------------------------------------------------------------
+numeroMultiplicar = 1; //<--- Indique o número a ser multiplicado
+//---------------------------------------------------------------
+console.log("=-=-=Tábuada Altomática=-=-=");
+for (let i = 0; i<11; i++){
+    console.log(`\t${numeroMultiplicar} x ${i} = ${numeroMultiplicar*i}`)
+};
+console.log("=-=-=-=-=-=-=-=-=-=-=-=by:jp");
