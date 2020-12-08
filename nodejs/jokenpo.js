c1da779f59d214ee489b439876116924bca4e7da
Joao Pedro <leopoldino26@gmail.com>
2020-12-08 09:19:58 -0300

    Tarefa-Jokenpo

--- nodejs/jokenpo.js
+++ nodejs/jokenpo.js
@@ -1,13 +1,31 @@
 const http = require('http');
 const url = require('url');
 
+const jogadas = ['pedra', 'papel', 'tesoura']
+
 const cpu = () =>{
-    
+    const aleatorio = parseInt(Math.random()*3);
+    return jogadas[aleatorio];
+};
+
+const vencedor = (player, cpu) =>{
+    if(player==="tesoura" && cpu==="papel" ||
+       player==="papel" && cpu==="pedra"   ||
+       player==="pedra" && cpu==="tesoura"){
+        return "<h1>Voce Ganhou!!!</h1>";
+    } else if(player===cpu){
+        return "<h1>Empatou!</h1>"
+    } else {
+        return "<h1>Voce Perdeu :(</h1>"
+    }
 };
 
 const servidor = http.createServer((req, res)=>{
     const resultadoUrl = url.parse(req.url, true).query;
-    res.write();
+    const escolhaCpu = cpu();
+    const escolhaPlayer = resultadoUrl.jogada;
+    res.write(`<h1>User: ${escolhaPlayer} | PC: ${escolhaCpu}</h1>`);
+    res.write(vencedor(escolhaPlayer, escolhaCpu));
     res.end();
 })
 
